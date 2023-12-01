7da46879e1d2645d103763d6b5e359e017c0e1e6
wonjun <wonjon0930@daum.net>
2023-12-01 13:49:03 +0900

    feat: add feature to print statement to ask user to insert participant names

--- src/main/java/view/output/OutputView.java
+++ src/main/java/view/output/OutputView.java
@@ -1,8 +1,14 @@
 package view.output;
 
+import static view.output.constant.OutputMessageConstant.INSERT_PARTICIPANT_NAME;
 import static view.output.constant.OutputSymbolConstant.NEW_LINE;
 
 public class OutputView {
+
+    public void askToInsertParticipantNames() {
+        print(INSERT_PARTICIPANT_NAME.getMessage());
+        printLine();
+    }
 
     private void print(String message) {
         System.out.print(message);
--- src/main/java/view/output/constant/OutputMessageConstant.java
+++ src/main/java/view/output/constant/OutputMessageConstant.java
@@ -1,7 +1,7 @@
 package view.output.constant;
 
 public enum OutputMessageConstant {
-    ;
+    INSERT_PARTICIPANT_NAME("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     private final String message;
 
     OutputMessageConstant(final String message) {
