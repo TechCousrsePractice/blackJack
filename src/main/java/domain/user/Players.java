9c6a22b28cf76702913a53364e38fcd6ee6187c8
noah <jeongas9810@gmail.com>
2023-12-05 01:28:56 +0900

    feat: Add player's wrapper class Players

---
+++ src/main/java/domain/user/Players.java
@@ -0,0 +1,11 @@
+package domain.user;
+
+import java.util.List;
+
+public class Players {
+    private List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+}
